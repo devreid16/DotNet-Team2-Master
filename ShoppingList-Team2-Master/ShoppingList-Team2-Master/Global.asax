7afa568acb5e472a8c1d85d48a45db6a32c0de2c
Hamsandwitch <pddobso@gmail.com>
2016-09-07 09:59:50 -0400

    initial commit/push
    +added git ignore file
    
    +post-built

---
+++ ShoppingList-Team2-Master/ShoppingList-Team2-Master/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ShoppingList_Team2_Master.MvcApplication" Language="C#" %>
---
+++ ShoppingList-Team2-Master/ShoppingList-Team2-Master/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShoppingList_Team2_Master.Startup))]
+namespace ShoppingList_Team2_Master
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
