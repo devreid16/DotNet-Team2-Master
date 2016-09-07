80fe3168449f2e69a702ce81077c7fe0a5c8e624
Angela Reid <reidang94@gmail.com>
2016-09-07 13:38:43 -0400

    Added class properties for shopping list item model.

--- ShoppingList-Team2-Master/ShoppingList-Team2-Master/Models/ShoppingListItemModel.cs
+++ ShoppingList-Team2-Master/ShoppingList-Team2-Master/Models/ShoppingListItemModel.cs
@@ -5,7 +5,34 @@ using System.Web;
 
 namespace ShoppingList_Team2_Master.Models
 {
-     public class ShoppingListItemModel
+     public enum Priority
      {
+          A, B, C
+     }
+          
+     public class ShoppingListItemModel
+     {          
+          
+          public int ListId { get; set; }
+
+          public string Name { get; set; }
+
+          public bool IsChecked { get; set; }
+
+          public bool Purchased { get; set; }
+
+          public string Contents { get; set; }
+
+          public Priority? Priority { get; set; }
+
+          public string Note { get; set; }
+
+          public DateTimeOffset CreatedUtc { get; set; }
+
+          //get list id and name
+          public override string ToString()
+          {
+               return $"[{ListId} {Name}";
+          }
      }
 }
